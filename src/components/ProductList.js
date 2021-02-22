dfe7ee86563da29e9c91671417b1347fc945e84e
lailaalhaddad <lalhaddad95@gmail.com>
2021-02-22 17:33:36 +0300

    Done

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,15 +5,21 @@ import ProductItem from "./ProductItem";
 import SearchBar from "./SearchBar";
 // Data
 import products from "../products";
+import { useState } from "react";
 
 const ProductList = () => {
-  const productList = products.map((product) => (
+  const [query, setQuery] = useState("");
+  const filteredproducts = products.filter((product) =>
+    product.name.includes(query)
+  );
+  const productList = filteredproducts.map((product) => (
     <ProductItem product={product} key={product.id} />
   ));
+  console.log("filteredproducts", filteredproducts);
 
   return (
     <>
-      <SearchBar />
+      <SearchBar setQuery={setQuery} />
       <ListWrapper>{productList}</ListWrapper>
     </>
   );
